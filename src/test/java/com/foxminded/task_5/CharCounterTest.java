97793693293bffa19710ed7cb152fb5cca968f53
emrys <emriesy@gmail.com>
2021-07-16 23:24:37 +0200

    add one test

--- src/test/java/com/foxminded/task_5/CharCounterTest.java
+++ src/test/java/com/foxminded/task_5/CharCounterTest.java
@@ -12,10 +12,12 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CharCounterTest {
-    private final static String INPUT_WORD = "hell";
+    private final static String INPUT_WORD_UPPER_KEYS = "HELL";
+    private final static String INPUT_WORD_LOWER_KEYS = "hell";
     private CharCounter charCounter;
     private Formatter formatter;
-    private Cash cash;
+    private Cash cashLower;
+    private Cash cashUpper;
     private HashMap<Character, Integer> cashMap;
     @Mock
     Calculator calculator;
@@ -26,16 +28,31 @@ public class CharCounterTest {
         cashMap.put('h', 1);
         cashMap.put('e', 1);
         cashMap.put('l', 2);
-        cash = new Cash(cashMap, INPUT_WORD);
+        cashLower = new Cash(cashMap, INPUT_WORD_LOWER_KEYS);
+        cashMap.clear();
+        cashMap.put('H', 1);
+        cashMap.put('E', 1);
+        cashMap.put('L', 2);
+        cashUpper = new Cash(cashMap, INPUT_WORD_UPPER_KEYS);
         formatter = new Formatter();
         charCounter = new CharCounter(calculator, formatter);
     }
 
     @Test
     void cashReturnedIfSimilarInput() {
-        when(calculator.fillMap(INPUT_WORD)).thenReturn(cash);
-        charCounter.buildString(INPUT_WORD);
-        charCounter.buildString(INPUT_WORD);
-        verify(calculator, times(1)).fillMap(INPUT_WORD);
+        when(calculator.fillMap(INPUT_WORD_LOWER_KEYS)).thenReturn(cashLower);
+        charCounter.buildString(INPUT_WORD_LOWER_KEYS);
+        charCounter.buildString(INPUT_WORD_LOWER_KEYS);
+        verify(calculator, times(1)).fillMap(INPUT_WORD_LOWER_KEYS);
+    }
+
+    @Test
+    void cashNotReturnedIfSimilarInputLowerAndUpperKeys() {
+        when(calculator.fillMap(INPUT_WORD_LOWER_KEYS)).thenReturn(cashLower);
+        when(calculator.fillMap(INPUT_WORD_UPPER_KEYS)).thenReturn(cashUpper);
+        charCounter.buildString(INPUT_WORD_LOWER_KEYS);
+        charCounter.buildString(INPUT_WORD_UPPER_KEYS);
+        verify(calculator, times(1)).fillMap(INPUT_WORD_LOWER_KEYS);
+        verify(calculator, times(1)).fillMap(INPUT_WORD_UPPER_KEYS);
     }
 }
